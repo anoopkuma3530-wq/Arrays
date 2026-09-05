073d2b27a6628c81bdaacf5c71ca5a92c6ce9240
Anoop Kumar <anoopkuma3530@gmail.com>
2026-09-05 16:31:36 +0530

    initial commit

---
+++ src/patten_printing/Bridge.java
@@ -0,0 +1,32 @@
+package patten_printing;
+
+import java.util.Scanner;
+
+public class Bridge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int n = sc.nextInt();
+        for (int i=1;i<=2*n-1;i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        int nsp = 1;
+        for(int i=1;i<=n-1;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*"+" ");
+
+            }
+            for(int j=1;j<=nsp;j++) {
+                System.out.print(" " + " ");
+            }
+            for(int j=1;j<=n-i;j++) {
+                System.out.print("*" + " ");
+            }
+            nsp +=2;
+            System.out.println();
+
+        }
+
+    }
+}
